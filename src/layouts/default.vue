50f5518daf0e9e1147c755d614c1648fb4428cd4
Wisansiiz <18257719723@163.com>
2024-02-01 19:26:28 +0800

    feat: 主页样式

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,161 @@
+<script lang="ts">
+import type { Component } from 'vue'
+import { defineComponent, h } from 'vue'
+import { NIcon } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+import { RouterLink } from 'vue-router'
+import {
+  BookOutline as BookIcon,
+  HomeOutline as HomeIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon,
+} from '@vicons/ionicons5'
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions: MenuOption[] = [
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            path: '/',
+          },
+        },
+        { default: () => '回家' },
+      ),
+    key: 'go-back-home',
+    icon: renderIcon(HomeIcon),
+  },
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            path: '/ws',
+          },
+        },
+        { default: () => '房间' },
+      ),
+    key: 'go-room',
+    icon: renderIcon(BookIcon),
+  },
+  {
+    label: '舞，舞，舞',
+    key: 'dance-dance-dance',
+    icon: renderIcon(BookIcon),
+    children: [
+      {
+        type: 'group',
+        label: '人物',
+        key: 'people',
+        children: [
+          {
+            label: '叙事者',
+            key: 'narrator',
+            icon: renderIcon(PersonIcon),
+          },
+          {
+            label: '羊男',
+            key: 'sheep-man',
+            icon: renderIcon(PersonIcon),
+          },
+        ],
+      },
+      {
+        label: '饮品',
+        key: 'beverage',
+        icon: renderIcon(WineIcon),
+        children: [
+          {
+            label: '威士忌',
+            key: 'whisky',
+          },
+        ],
+      },
+      {
+        label: '食物',
+        key: 'food',
+        children: [
+          {
+            label: '三明治',
+            key: 'sandwich',
+          },
+        ],
+      },
+      {
+        label: '过去增多，未来减少',
+        key: 'the-past-increases-the-future-recedes',
+      },
+    ],
+  },
+]
+
+export default defineComponent({
+  setup() {
+    return {
+      collapsed: ref(true),
+      menuOptions,
+    }
+  },
+})
+</script>
+
 <template>
-  <main
-    px-4 py-10
-    text="center gray-700 dark:gray-200"
-  >
-    <RouterView />
-    <TheFooter />
-    <div mx-auto mt-5 text-center text-sm opacity-50>
-      [Default Layout]
-    </div>
-  </main>
+  <div>
+    <n-layout position="absolute">
+      <n-layout-header bordered style="max-height: 64px">
+        <TheHeader />
+        <n-menu
+          mode="horizontal" :options="menuOptions"
+        />
+      </n-layout-header>
+      <n-layout has-sider position="absolute" style="top: 64px">
+        <n-layout-sider
+          bordered
+          collapse-mode="width"
+          :collapsed-width="64"
+          :width="240"
+          :collapsed="collapsed"
+          show-trigger
+          @collapse="collapsed = true"
+          @expand="collapsed = false"
+        >
+          <n-menu
+            :collapsed="collapsed"
+            :collapsed-width="64"
+            :collapsed-icon-size="22"
+            :options="menuOptions"
+          />
+        </n-layout-sider>
+        <n-layout has-sider sider-placement="right">
+          <n-layout content-style="padding: 24px;" :native-scrollbar="false">
+            <RouterView />
+          </n-layout>
+          <n-layout-sider
+            bordered
+            show-trigger
+            collapse-mode="width"
+            :collapsed-width="64"
+            :width="240"
+            :native-scrollbar="false"
+          >
+            <n-menu
+              :collapsed-width="64"
+              :collapsed-icon-size="22"
+              :options="menuOptions"
+            />
+          </n-layout-sider>
+        </n-layout>
+      </n-layout>
+    </n-layout>
+  </div>
 </template>
+
+<style scoped>
+
+</style>
