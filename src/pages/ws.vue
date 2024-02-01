2c2737782b83c02f4e96b0c1aa1ab426fae30b4d
Wisansiiz <18257719723@163.com>
2024-02-01 19:27:52 +0800

    style: 样式修改

--- src/pages/ws.vue
+++ src/pages/ws.vue
@@ -9,21 +9,23 @@ export default {
     const messages = ref([])
     const isChatBoxVisible = ref(false)
     let ws
+    const active = ref(false)
 
     const joinRoom = () => {
       if (roomID.value.trim() !== '') {
-        ws = new WebSocket(`ws://localhost:8080/ws?roomID=${roomID.value.trim()}`)
+        ws = new WebSocket(`ws://localhost:9000/api/ws?roomID=${roomID.value.trim()}`)
         messages.value = []
         ws.onmessage = (event) => {
           const message = JSON.parse(event.data)
           messages.value.push(message)
         }
         isChatBoxVisible.value = true
+        active.value = true
       }
     }
     const queryHistoryMessages = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/history?roomID=${roomID.value.trim()}`)
+        const response = await axios.get(`http://localhost:9000/api/history?roomID=${roomID.value.trim()}`)
         messages.value = response.data.data.map((event) => {
           return JSON.parse(event.text)
         })
@@ -47,34 +49,52 @@ export default {
       joinRoom,
       sendMessage,
       queryHistoryMessages,
+      active,
     }
   },
 }
 </script>
 
 <template>
-  <div>
-    <div v-show="isChatBoxVisible">
-      <ul>
-        <li v-for="(message, index) in messages" :key="index">
-          {{ message.data }}
-        </li>
-      </ul>
-      <textarea v-model="messageText" placeholder="Type your message" />
-      <button @click="sendMessage">
-        Send
-      </button>
-    </div>
-    <input v-model="roomID" type="text" placeholder="Enter room ID">
-    <button @click="joinRoom">
-      Join Room
-    </button>
-    <div>
-      <button @click="queryHistoryMessages">
+  <n-flex>
+    <n-scrollbar style="margin-bottom: 50px">
+      <n-flex v-show="isChatBoxVisible" justify="center">
+        <n-scrollbar style="max-width: 300px">
+          <n-list clickable hoverable>
+            <n-list-item v-for="(message, index) in messages" :key="index">
+              {{ message.data }}
+            </n-list-item>
+          </n-list>
+        </n-scrollbar>
+      </n-flex>
+      <n-input
+        v-model:value="roomID"
+        type="text"
+        placeholder="Enter room ID"
+        :disabled="active"
+      />
+      <n-button @click="joinRoom">
+        Join Room
+      </n-button>
+      <n-button @click="queryHistoryMessages">
         Search
-      </button>
-    </div>
-  </div>
+      </n-button>
+    </n-scrollbar>
+    <n-layout-footer
+      bordered
+      position="absolute"
+      style="height: 64px;padding: 5px"
+    >
+      <n-flex justify="center">
+        <TheInput v-model:value="messageText" />
+        <div style="height: 54px; line-height: 54px">
+          <n-button type="primary" ghost @click="sendMessage()">
+            发送
+          </n-button>
+        </div>
+      </n-flex>
+    </n-layout-footer>
+  </n-flex>
 </template>
 
 <style scoped>
