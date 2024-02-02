ee4364c12dd3d8c7050804c9e2208b5febf5ea74
Wisansiiz <18257719723@163.com>
2024-02-02 23:08:01 +0800

    style: 样式修改

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,8 +7,6 @@ import { RouterLink } from 'vue-router'
 import {
   BookOutline as BookIcon,
   HomeOutline as HomeIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon,
 } from '@vicons/ionicons5'
 
 function renderIcon(icon: Component) {
@@ -45,53 +43,32 @@ const menuOptions: MenuOption[] = [
     icon: renderIcon(BookIcon),
   },
   {
-    label: '舞，舞，舞',
-    key: 'dance-dance-dance',
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            path: '/server',
+          },
+        },
+        { default: () => '123房间' },
+      ),
+    key: 'go-123room',
     icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: 'group',
-        label: '人物',
-        key: 'people',
-        children: [
-          {
-            label: '叙事者',
-            key: 'narrator',
-            icon: renderIcon(PersonIcon),
+  },
+  {
+    label: () =>
+      h(
+        RouterLink,
+        {
+          to: {
+            path: '/login',
           },
-          {
-            label: '羊男',
-            key: 'sheep-man',
-            icon: renderIcon(PersonIcon),
-          },
-        ],
-      },
-      {
-        label: '饮品',
-        key: 'beverage',
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: '威士忌',
-            key: 'whisky',
-          },
-        ],
-      },
-      {
-        label: '食物',
-        key: 'food',
-        children: [
-          {
-            label: '三明治',
-            key: 'sandwich',
-          },
-        ],
-      },
-      {
-        label: '过去增多，未来减少',
-        key: 'the-past-increases-the-future-recedes',
-      },
-    ],
+        },
+        { default: () => '登录' },
+      ),
+    key: '',
+    icon: renderIcon(BookIcon),
   },
 ]
 
