7acb374c530f5fb7d4b2ac3a153c3812f8b018c8
Wisansiiz <18257719723@163.com>
2024-02-06 12:07:56 +0800

    fix: 登录注册方法

--- src/composables/authorized.js
+++ src/composables/authorized.js
@@ -1,72 +1,47 @@
-import axios from 'axios'
-import { useAuthStore } from '~/composables/auth.js'
-import { gLoadingBarDone } from '~/composables/gDiscreteApi'
+import { baseURL, service } from '~/composables/interceptor.js'
 
-const baseURL = 'http://localhost:9000/api'
-const service = axios.create({ baseURL, timeout: 5000 })
-const errMsg = '服务异常'
-const authStore = useAuthStore()
-service.interceptors.request.use((config) => {
-  const authStore = useAuthStore()
-  if (authStore.token)
-    config.headers.Authorization = `Bearer ${authStore.token}`
-  return config
-})
-service.interceptors.response.use(
-  (result) => {
-    if (result.data.code === 200) {
-      return result.data
-    }
-    else {
-      authStore.token = null
-      return result.data
-    }
-    // router.push('/login').then(() => {
-    //   gMessage.warning('登录已失效，请重新登录')
-    // })
-    // gMessage.error(result.data.msg || errMsg)
-    // gLoadingBarDone.error()
-    // return Promise.reject(result.data)
-  },
-  (err) => {
-    gMessage.error(errMsg)
-    gLoadingBarDone.error()
-    return Promise.reject(err)
-  },
-)
+const localStore = useAuthLocalStore()
+const sessionStore = useAuthSessionStore()
 
-export function isOnline(success) {
-  service.post(`${baseURL}/auth`).then((data) => {
-    if (data.code === 200 && data.data.expires > Math.round(new Date().getTime() / 1000)) {
+function ok(code) {
+  return /^2[0-9][0-9]$/.test(code)
+}
+
+export async function isOnline(success) {
+  await service.post(`${baseURL}/auth`).then((res) => {
+    if (ok(res.code)) {
       success(true)
     }
     else {
       success(false)
       gMessage.warning('登录已失效，请重新登录')
     }
+  }).catch(() => {
+    success(false)
+    gMessage.warning('登录已失效，请重新登录')
   })
 }
 
-export { service }
+export async function register({ email, password, username }) {
+  return await service.post('/register', { email, password, username })
+}
 
 export async function login({ username, password }) {
-  const res = await service.post(
+  return await service.post(
     '/login',
     { username, password },
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
   )
-  return res.data
 }
 
 export async function logout() {
-  const authStore = useAuthStore()
-  authStore.token = null
-  // router.push('/login').then(() => {
-  gMessage.warning('登录已失效，请重新登录')
-  // })
-}
-
-export async function register({ email, password, name }) {
-  const res = await service.post('/register', { email, password, name })
-  return res.data
+  await service.post('/logout').then((res) => {
+    if (!ok(res.code)) {
+      gMessage.error(res.msg)
+      return
+    }
+    localStore.token = null
+    sessionStore.token = null
+    gMessage.success(res.msg)
+  })
 }
