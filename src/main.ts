5608cfc9ba989eb6a1233293dfaafd549fd6944a
Wisansiiz <18257719723@163.com>
2024-02-10 15:42:45 +0800

    chore: 重构整个项目

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { setupLayouts } from 'virtual:generated-layouts'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import App from './App.vue'
+
+// import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+const app = createApp(App)
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    extendRoutes: routes => setupLayouts(routes),
+})
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+gLoadingBar(router)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import UnoCSS from 'unocss/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+import Layouts from 'vite-plugin-vue-layouts'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Pages from 'vite-plugin-pages'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+    Vue(),
+    VueRouter(),
+    Layouts(),
+    Pages(),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        VueRouterAutoImports,
+        {
+          'vue-router/auto': ['useLink'],
+        },
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar',
+          ],
+        },
+      ],
+      dts: true,
+      dirs: [
+        './src/composables',
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: true,
+      resolvers: [NaiveUiResolver()],
+    }),
+    UnoCSS(),
+  ],
+})
