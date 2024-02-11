3142df935e6eac0a3165d95b052cb4ec7c7e061e
Wisansiiz <18257719723@163.com>
2024-02-11 17:54:09 +0800

    feat: 创建服务器

---
+++ src/components/TheChannelList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <n-layout has-sider style="max-width: 240px; border-right: 1px solid #e8e8e8">
+
+  </n-layout>
+</template>
+
+<style scoped>
+
+</style>
