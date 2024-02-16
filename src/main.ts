86a09c3d5c16b8abbf42ac5ec96e0a09ed46def8
Wisansiiz <18257719723@163.com>
2024-02-16 16:39:37 +0800

    style: 格式

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    extendRoutes: routes => setupLayouts(routes),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  extendRoutes: routes => setupLayouts(routes),
 })
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
