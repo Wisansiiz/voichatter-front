8ed8c9512d82edbab7dc630fcd1b7108486ebf14
Wisansiiz <18257719723@163.com>
2024-02-16 16:47:55 +0800

    chore: 依赖导入

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -43,9 +43,10 @@ declare module 'vue-router/auto/routes' {
     '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
     '/hi/[name]': RouteRecordInfo<'/hi/[name]', '/hi/:name', { name: ParamValue<true> }, { name: ParamValue<false> }>,
     '/login': RouteRecordInfo<'/login', '/login', Record<never, never>, Record<never, never>>,
+    '/multiple': RouteRecordInfo<'/multiple', '/multiple', Record<never, never>, Record<never, never>>,
     '/register': RouteRecordInfo<'/register', '/register', Record<never, never>, Record<never, never>>,
     '/server': RouteRecordInfo<'/server', '/server', Record<never, never>, Record<never, never>>,
-    '/test': RouteRecordInfo<'/test', '/test', Record<never, never>, Record<never, never>>,
+    '/single': RouteRecordInfo<'/single', '/single', Record<never, never>, Record<never, never>>,
     '/ws': RouteRecordInfo<'/ws', '/ws', Record<never, never>, Record<never, never>>,
   }
 }
