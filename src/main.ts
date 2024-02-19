13762d4013dc93bde27a1bedd11cbe9a51c8d87a
Wisansiiz <18257719723@163.com>
2024-02-19 20:04:22 +0800

    feat: 提取router用于全局调用

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
+import { router } from '~/router'
 
 // import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
 
 const app = createApp(App)
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: routes => setupLayouts(routes),
-})
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 gLoadingBar(router)
