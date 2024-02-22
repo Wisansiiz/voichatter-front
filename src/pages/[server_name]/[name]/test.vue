dd3e5422bc7422667c90979b4d0271eec7dbc0be
Wisansiiz <18257719723@163.com>
2024-02-22 20:51:22 +0800

    refactor: 修改路径

---
+++ src/pages/[server_name]/[name]/test.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const params = useRoute('/server/channel/[name]').params
+</script>
+
+<template>
+  <n-h1>{{ params.name }}</n-h1>
+</template>
+
+<style scoped>
+
+</style>
