2d9aa3fdcf9a9489d84b8fb86ad9346b8f34ca0c
Wisansiiz <18257719723@163.com>
2024-02-22 20:46:44 +0800

    feat: 读取服务器名称

---
+++ src/pages/[server_name].vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps<{
+  serverName: string
+}>()
+const route = useRoute()
+const servername = ref('默认值')
+watch(route, () => {
+  servername.value = props.serverName
+})
+</script>
+
+<template>
+  <TheChannelList :server-name="servername" />
+  <n-layout has-sider sider-placement="right">
+    <n-layout content-style="padding: 24px;" :native-scrollbar="false">
+      <RouterView />
+    </n-layout>
+    <TheInfoMenu />
+  </n-layout>
+</template>
+
+<style scoped>
+
+</style>
