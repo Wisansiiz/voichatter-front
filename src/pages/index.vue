77224be45b3873f295242b32d0e500694632877f
Wisansiiz <18257719723@163.com>
2024-02-24 22:12:23 +0800

    style: 样式

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 defineOptions({
   name: 'IndexPage',
 })
-const router = useRouter()
+
 const messages = [
   { username: '张三', text: '一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，一段非常长的文本，', isSent: true, time: '2024年2月19日 13:55', avatar: 'https://thirdqq.qlogo.cn/g?b=qq&nk=1392634254&s=100' },
   { username: '李四', text: '一段非常长的文本，一段非常长的文本，一段非常长的文本一段非常长的文本，一段非常长的文本，一段非常长的文本', isSent: false, time: '2024年2月19日 13:56', avatar: 'https://thirdqq.qlogo.cn/g?b=qq&nk=1392634254&s=100' },
@@ -15,18 +15,18 @@ const messages = [
     <n-button @click="logout()">
       退出登录
     </n-button>
+    <div class="chat-window">
+      <ChatBubble
+        v-for="(msg, index) in messages"
+        :key="index"
+        :message="msg.text"
+        :is-sent="msg.isSent"
+        :avatar="msg.avatar"
+        :time="msg.time"
+        :username="msg.username"
+      />
+    </div>
   </n-scrollbar>
-  <div class="chat-window">
-    <ChatBubble
-      v-for="(msg, index) in messages"
-      :key="index"
-      :message="msg.text"
-      :is-sent="msg.isSent"
-      :avatar="msg.avatar"
-      :time="msg.time"
-      :username="msg.username"
-    />
-  </div>
 </template>
 
 <style scoped>
