ab153c8fb93f9a5b6f18865933c6c4f4e5c722ec
Wisansiiz <18257719723@163.com>
2024-03-13 22:30:32 +0800

    chore: 依赖更新

--- components.d.ts
+++ components.d.ts
@@ -29,6 +29,8 @@ declare module 'vue' {
     NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
+    NCollapse: typeof import('naive-ui')['NCollapse']
+    NCollapseItem: typeof import('naive-ui')['NCollapseItem']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDataTable: typeof import('naive-ui')['NDataTable']
     NDialogProvider: typeof import('naive-ui')['NDialogProvider']
