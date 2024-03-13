146b02d78db45af0ddb6f5de75944a4a707801ff
Wisansiiz <18257719723@163.com>
2024-03-13 22:16:48 +0800

    feat: 脱离上下文的api

---
+++ type.d.ts
@@ -0,0 +1,14 @@
+import type { DialogApiInjection } from 'naive-ui/es/dialog/src/DialogProvider'
+import type { LoadingBarInst } from 'naive-ui/es/loading-bar/src/LoadingBarProvider'
+import type { MessageApiInjection } from 'naive-ui/es/message/src/MessageProvider'
+import type { NotificationApiInjection } from 'naive-ui/es/notification/src/NotificationProvider'
+
+export {}
+declare global {
+  interface Window {
+    $dialog: DialogApiInjection
+    $loadingBar: LoadingBarInst
+    $message: MessageApiInjection
+    $notification: NotificationApiInjection
+  }
+}
