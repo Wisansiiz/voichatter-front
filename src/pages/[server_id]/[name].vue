1e189ca5ac800b2a9a1b0ffd8e591547fbb555bc
Wisansiiz <18257719723@163.com>
2024-03-13 22:14:21 +0800

    feat: 根据type显示不同页面

--- src/pages/[server_id]/[name].vue
+++ src/pages/[server_id]/[name].vue
@@ -9,6 +9,7 @@
 // }
 import Ws from '~/pages/[server_id]/[name]/ws.vue'
 import Multiple from '~/pages/[server_id]/[name]/multiple.vue'
+import { useServerInfo } from '~/hooks/useServerInfo'
 
 export default defineComponent({
   components: { Ws, Multiple },
@@ -17,10 +18,12 @@ export default defineComponent({
     const route = useRoute()
     const params = ref()
     params.value = route.params
+    const { channelType } = useServerInfo()
     watch(route, () => {
       params.value = route.params
     })
     return {
+      channelType,
       params,
       router,
       Ws,
@@ -34,53 +37,23 @@ export default defineComponent({
 
   <n-flex>
     <n-h1>{{ params.name }}</n-h1>
-    <!--    <n-scrollbar style="margin-bottom: 50px"> -->
-    <!--    <n-button @click="router.push(`${params.name}/ws`)"> -->
-    <!--      发送 -->
-    <!--    </n-button> -->
-    <!--    <RouterView /> -->
-    <!--      <n-input -->
-    <!--        value="123" -->
-    <!--        type="text" -->
-    <!--        :disabled="true" -->
-    <!--      /> -->
-    <!--      <n-button @click="queryHistoryMessages"> -->
-    <!--        Search -->
-    <!--      </n-button> -->
-    <!--      <n-flex justify="center"> -->
-    <!--        <n-scrollbar style="max-width: 300px"> -->
-    <!--          <div class="chat-window"> -->
-    <!--            <ChatBubble -->
-    <!--              v-for="(msg, index) in messages" -->
-    <!--              :key="index" -->
-    <!--              :message="msg.content" -->
-    <!--              :is-sent="msg.isSent" -->
-    <!--              avatar="https://thirdqq.qlogo.cn/g?b=qq&nk=1392634254&s=100" -->
-    <!--              :time="formatTime(msg.send_date)" -->
-    <!--              username="测试" -->
-    <!--            /> -->
-    <!--          </div> -->
-    <!--        </n-scrollbar> -->
-    <!--      </n-flex> -->
-    <!--    </n-scrollbar> -->
-    <!--    <n-layout-footer -->
-    <!--      bordered -->
-    <!--      position="absolute" -->
-    <!--      style="height: 64px;padding: 5px" -->
-    <!--    > -->
-    <!--      <n-flex justify="center"> -->
-    <!--        <TheInput v-model:value="messageText" /> -->
-    <!--        <div style="height: 54px; line-height: 54px"> -->
-    <!--          <n-button type="primary" ghost @click="sendMessage()"> -->
-    <!--            发送 -->
-    <!--          </n-button> -->
-    <!--        </div> -->
-    <!--      </n-flex> -->
-    <!--    </n-layout-footer> -->
+    <n-layout-footer
+      bordered
+      position="absolute"
+      style="height: 64px;padding: 5px"
+    >
+      <n-flex justify="center">
+        <div style="height: 54px; line-height: 54px">
+          <n-button type="primary" ghost>
+            发送{{ params.name }}
+          </n-button>
+        </div>
+      </n-flex>
+    </n-layout-footer>
   </n-flex>
 
-  <Ws />
-  <Multiple />
+  <Ws v-if="channelType === 'text'" />
+  <Multiple v-if="channelType === 'voice'" />
 </template>
 
 <style scoped>
