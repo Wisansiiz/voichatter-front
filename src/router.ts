45bbeb34a21495bba58788266b865bf25d08b5bd
Wisansiiz <18257719723@163.com>
2024-03-13 22:20:38 +0800

    feat: router

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
+import type { App } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   extendRoutes: routes => setupLayouts(routes),
 })
+export function setupRouter(app: App) {
+  app.use(router)
+  // 创建路由守卫
+  createRouterGuards(router)
+}
 export { router }
+function createRouterGuards(router: any) {
+  router.beforeEach(() => {
+    window.$loadingBar.start()
+  })
+
+  router.afterEach(() => {
+    window.$loadingBar.finish()
+  })
+
+  router.onError(() => {
+    window.$loadingBar.error()
+  })
+}
