7a344133ec28a7c1ad652c19852cb54080f7ede3
Wisansiiz <18257719723@163.com>
2024-03-13 22:27:39 +0800

    feat: util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -38,3 +38,17 @@ export function createImageVNode(url: string, name: string) {
 export function renderIcon(icon: any) {
   return () => h(NIcon, null, { default: () => h(icon) })
 }
+function addLight(color: string, amount: number) {
+  const cc = Number.parseInt(color, 16) + amount
+  const c = cc > 255 ? 255 : cc
+  return c.toString(16).length > 1 ? c.toString(16) : `0${c.toString(16)}`
+}
+
+export function lighten(color: string, amount: number) {
+  color = color.includes('#') ? color.substring(1, color.length) : color
+  amount = Math.trunc((255 * amount) / 100)
+  return `#${addLight(color.substring(0, 2), amount)}${addLight(
+    color.substring(2, 4),
+    amount,
+  )}${addLight(color.substring(4, 6), amount)}`
+}
