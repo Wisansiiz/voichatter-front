165d3cc854d749fb2a480167947de229a3dbd307
Wisansiiz <18257719723@163.com>
2024-03-23 22:14:05 +0800

    style: 样式修改

--- src/pages/[server_id].vue
+++ src/pages/[server_id].vue
@@ -38,17 +38,17 @@
 
 <style scoped lang="less">
 .layout-sider {
-  min-height: 90vh;
+  min-height: 91vh;
   box-shadow: 2px 0 8px 0 rgb(29 35 41 / 5%);
   position: relative;
   transition: all 0.2s ease-in-out;
 }
 .layout-content {
-  max-height: 90vh;
+  max-height: 91vh;
   min-height: 40vh;
   padding: 10px;
 }
 .layout {
-  max-height: 90vh;
+  max-height: 91vh;
 }
 </style>
