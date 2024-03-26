7d33f1903da0c862e11117e3d73066fa1d554ec4
Wisansiiz <18257719723@163.com>
2024-03-26 22:38:07 +0800

    feat: 提取和优化

--- src/components/Multiple.vue
+++ src/components/Multiple.vue
@@ -1,191 +1,231 @@
 <script lang="ts">
-import { useUserStore } from '~/store/modules/user'
-import { wssBase } from '~/api'
+// import { useUserStore } from '~/store/modules/user'
+// import { wssBase } from '~/api'
+
+import { useWebsocketStore } from '~/store/modules/websocket'
 
 export default defineComponent({
   setup() {
     const videoContainer = ref()
-    const socket: Ref<WebSocket | null> = ref(null)
-    const pcMap = new Map()
-    let localStream: MediaStream
+    // const socket: Ref<WebSocket | null> = ref(null)
+    // const pcMap = new Map()
+    // let localStream: MediaStream
     const localVideo = ref()
-    const userStore = useUserStore()
-    const route: any = useRoute()
+    // const userStore = useUserStore()
+    // const route: any = useRoute()
 
-    async function createWebsocket() {
-      return new Promise((resolve) => {
-        // try {
-        if (socket.value) {
-          socket.value.close()
-          socket.value = null
-        }
-        socket.value = new WebSocket(`${wssBase}/api/yy?serverId=${route.params.server_id}&channelId=${route.params.name}&token=${encodeURIComponent(userStore.getToken)}`)
-        socket.value.onopen = () => {
-          window.$message.success('连接成功')
-          resolve(socket.value)
-        }
-        socket.value.onclose = () => {
-          window.$message.error('连接断开')
-        }
-      })
-    }
-    async function initWebsocket() {
-      await createWebsocket()
-      if (socket.value && socket.value.readyState === WebSocket.OPEN) {
-        socket.value.onmessage = async (e) => {
-          const message = e.data
-          const { code, data } = JSON.parse(message)
-          console.log(code)
-          if (code === 'offer') {
-            // 接收offer
-            await getOffer(data)
-          }
-          else if (code === 'answer') {
-            // 接收answer
-            await getAnswer(data)
-          }
-          else if (code === 'candidate') {
-            const { fromId, candidate } = data
-            const pc = pcMap.get(fromId)
-            pc.addIceCandidate(candidate)
-          }
-          else if (code === 'join_group') {
-            const { fromId } = data
-            await sendOffer(fromId)
-          }
-          else if (code === 'leave_group') {
-            const { fromId } = data
-            pcMap.delete(fromId)
-            const video = document.getElementById(fromId)
-            if (video)
-              video.remove()
-          }
-        }
-      }
-    }
-    async function handleJoin() {
-      await initWebsocket()
-      localStream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: {
-          echoCancellation: true,
-          noiseSuppression: true,
-        },
-      })
-      localVideo.value.srcObject = localStream
-      const message = {
-        code: 'join_group',
-      }
-      socket.value?.send(JSON.stringify(message))
-    }
+    // async function createWebsocket() {
+    //   return new Promise((resolve) => {
+    //     // try {
+    //     if (socket.value) {
+    //       socket.value.close()
+    //       socket.value = null
+    //     }
+    //     socket.value = new WebSocket(`${wssBase}/api/yy?serverId=${route.params.server_id}&channelId=${route.params.name}&token=${encodeURIComponent(userStore.getToken)}`)
+    //     socket.value.onopen = () => {
+    //       window.$message.success('连接成功')
+    //       resolve(socket.value)
+    //     }
+    //     socket.value.onclose = () => {
+    //       window.$message.error('连接断开')
+    //     }
+    //   })
+    // }
+    // async function initWebsocket() {
+    //   await createWebsocket()
+    //   if (socket.value && socket.value.readyState === WebSocket.OPEN) {
+    //     socket.value.onmessage = async (e) => {
+    //       const message = e.data
+    //       const { code, data } = JSON.parse(message)
+    //       console.log(code)
+    //       if (code === 'offer') {
+    //         // 接收offer
+    //         await getOffer(data)
+    //       }
+    //       else if (code === 'answer') {
+    //         // 接收answer
+    //         await getAnswer(data)
+    //       }
+    //       else if (code === 'candidate') {
+    //         const { fromId, candidate } = data
+    //         const pc = pcMap.get(fromId)
+    //         pc.addIceCandidate(candidate)
+    //       }
+    //       else if (code === 'join_group') {
+    //         const { fromId } = data
+    //         await sendOffer(fromId)
+    //       }
+    //       else if (code === 'leave_group') {
+    //         const { fromId } = data
+    //         pcMap.delete(fromId)
+    //         const video = document.getElementById(fromId)
+    //         if (video)
+    //           video.remove()
+    //       }
+    //     }
+    //   }
+    // }
+    // async function handleJoin() {
+    //   await initWebsocket()
+    //   localStream = await navigator.mediaDevices.getUserMedia({
+    //     video: true,
+    //     audio: {
+    //       echoCancellation: true,
+    //       noiseSuppression: true,
+    //     },
+    //   })
+    //   localVideo.value.srcObject = localStream
+    //   const message = {
+    //     code: 'join_group',
+    //   }
+    //   socket.value?.send(JSON.stringify(message))
+    // }
+    //
+    // async function sendOffer(targetId: any) {
+    //   const pc = new RTCPeerConnection({})
+    //   pcMap.set(targetId, pc)
+    //   localStream.getTracks().forEach(track => pc.addTrack(track, localStream))
+    //   const video = document.createElement('video')
+    //   video.id = targetId
+    //   video.autoplay = true
+    //   videoContainer.value.appendChild(video)
+    //   pc.addEventListener('track', (e) => {
+    //     // 更新远程的视频
+    //     video.srcObject = e.streams[0]
+    //   })
+    //
+    //   pc.addEventListener('icecandidate', (e) => {
+    //     if (e.candidate) {
+    //       const message = {
+    //         code: 'candidate',
+    //         data: {
+    //           targetId,
+    //           candidate: e.candidate,
+    //         },
+    //       }
+    //       socket.value?.send(JSON.stringify(message))
+    //     }
+    //   })
+    //   const description = await pc.createOffer()
+    //   await pc.setLocalDescription(description)
+    //   const message = {
+    //     code: 'offer',
+    //     data: {
+    //       targetId,
+    //       offer: description,
+    //     },
+    //   }
+    //   socket.value?.send(JSON.stringify(message))
+    // }
+    //
+    // // 接收offer
+    // async function getOffer({ fromId: targetId, offer }: any) {
+    //   const pc = new RTCPeerConnection({})
+    //   pcMap.set(targetId, pc)
+    //   localStream.getTracks().forEach(track => pc.addTrack(track, localStream))
+    //   const video = document.createElement('video')
+    //   video.id = targetId
+    //   video.autoplay = true
+    //   videoContainer.value.appendChild(video)
+    //   pc.addEventListener('track', (e) => {
+    //     // 更新远程的视频
+    //     video.srcObject = e.streams[0]
+    //   })
+    //
+    //   pc.addEventListener('icecandidate', (e) => {
+    //     if (e.candidate) {
+    //       const message = {
+    //         code: 'candidate',
+    //         data: {
+    //           targetId,
+    //           candidate: e.candidate,
+    //         },
+    //       }
+    //       socket.value?.send(JSON.stringify(message))
+    //     }
+    //   })
+    //   await pc.setRemoteDescription(offer)
+    //   const description = await pc.createAnswer()
+    //   await pc.setLocalDescription(description)
+    //   const message = {
+    //     code: 'answer',
+    //     data: {
+    //       targetId,
+    //       answer: description,
+    //     },
+    //   }
+    //   socket.value?.send(JSON.stringify(message))
+    // }
+    //
+    // async function getAnswer({ fromId, answer }: any) {
+    //   const pc = pcMap.get(fromId)
+    //   await pc.setRemoteDescription(answer)
+    // }
+    //
+    // // 挂断通话并通知服务器用户离开频道
+    // async function hangUp() {
+    //   // // 发送离开频道的消息给服务器
+    //   const message = {
+    //     code: 'leave_group',
+    //   }
+    //   socket.value?.send(JSON.stringify(message))
+    //   // 关闭所有与当前用户相关的RTCPeerConnections
+    //   for (const [key, pc] of pcMap.entries()) {
+    //     pc.close()
+    //     pcMap.delete(key)
+    //     const video = document.getElementById(key)
+    //     videoContainer.value.removeChild(video)
+    //   }
+    //   // 停止所有本地媒体流
+    //   if (localStream)
+    //     localStream.getTracks().forEach(track => track.stop())
+    // }
+    // onMounted(() => {
+    //   socket.value?.close()
+    // })
+    // window.addEventListener('beforeunload', (e) => {
+    //   // Cancel the event
+    //   e.preventDefault()
+    //   // Chrome requires returnValue to be set
+    //   e.returnValue = '自定义文本'
+    //   console.log('beforeunload')
+    // })
 
-    async function sendOffer(targetId: any) {
-      const pc = new RTCPeerConnection({})
-      pcMap.set(targetId, pc)
-      localStream.getTracks().forEach(track => pc.addTrack(track, localStream))
-      const video = document.createElement('video')
-      video.id = targetId
-      video.autoplay = true
-      videoContainer.value.appendChild(video)
-      pc.addEventListener('track', (e) => {
-        // 更新远程的视频
-        video.srcObject = e.streams[0]
-      })
+    const websocketStore = useWebsocketStore()
+    onMounted(() => {
+      websocketStore.setLocalVideo(localVideo.value)
+      websocketStore.setVideoContainer(videoContainer.value)
+    })
 
-      pc.addEventListener('icecandidate', (e) => {
-        if (e.candidate) {
-          const message = {
-            code: 'candidate',
-            data: {
-              targetId,
-              candidate: e.candidate,
-            },
-          }
-          socket.value?.send(JSON.stringify(message))
-        }
-      })
-      const description = await pc.createOffer()
-      await pc.setLocalDescription(description)
-      const message = {
-        code: 'offer',
-        data: {
-          targetId,
-          offer: description,
-        },
-      }
-      socket.value?.send(JSON.stringify(message))
-    }
+    onBeforeRouteUpdate(async (_to) => {
+      await websocketStore.hangUp()
+    })
+    onBeforeRouteLeave(async (_to, _from, next) => {
+      await websocketStore.hangUp()
+      next()
+    })
 
-    // 接收offer
-    async function getOffer({ fromId: targetId, offer }: any) {
-      const pc = new RTCPeerConnection({})
-      pcMap.set(targetId, pc)
-      localStream.getTracks().forEach(track => pc.addTrack(track, localStream))
-      const video = document.createElement('video')
-      video.id = targetId
-      video.autoplay = true
-      videoContainer.value.appendChild(video)
-      pc.addEventListener('track', (e) => {
-        // 更新远程的视频
-        video.srcObject = e.streams[0]
-      })
-
-      pc.addEventListener('icecandidate', (e) => {
-        if (e.candidate) {
-          const message = {
-            code: 'candidate',
-            data: {
-              targetId,
-              candidate: e.candidate,
-            },
-          }
-          socket.value?.send(JSON.stringify(message))
-        }
-      })
-      await pc.setRemoteDescription(offer)
-      const description = await pc.createAnswer()
-      await pc.setLocalDescription(description)
-      const message = {
-        code: 'answer',
-        data: {
-          targetId,
-          answer: description,
-        },
-      }
-      socket.value?.send(JSON.stringify(message))
-    }
-
-    async function getAnswer({ fromId, answer }: any) {
-      const pc = pcMap.get(fromId)
-      await pc.setRemoteDescription(answer)
-    }
-
-    // 挂断通话并通知服务器用户离开频道
-    async function hangUp() {
-      // // 发送离开频道的消息给服务器
-      const message = {
-        code: 'leave_group',
-      }
-      socket.value?.send(JSON.stringify(message))
-      // 关闭所有与当前用户相关的RTCPeerConnections
-      for (const [key, pc] of pcMap.entries()) {
-        pc.close()
-        pcMap.delete(key)
-        const video = document.getElementById(key)
-        videoContainer.value.removeChild(video)
-      }
-      // 停止所有本地媒体流
-      if (localStream)
-        localStream.getTracks().forEach(track => track.stop())
-    }
+    const isVideo = ref(false)
+    const isAudio = ref(true)
     return {
       videoContainer,
       localVideo,
-      handleJoin,
-      hangUp,
-      socket,
       show: () => {
-        console.log(socket.value?.readyState)
+        // console.log(websocketStore.getVideoContainer())
+      },
+      isVideo,
+      isAudio,
+      handleChange(value: boolean) {
+        window.$message.info(`Update value: ${value}`)
+      },
+      handleJoin: () => {
+        websocketStore.handleJoin(isVideo.value, isAudio.value)
+      },
+      hangUp: () => {
+        websocketStore.hangUp()
+      },
+      refresh: () => {
+        location.reload()
       },
     }
   },
@@ -193,21 +233,48 @@ export default defineComponent({
 </script>
 
 <template>
+  <n-layout-header relative style="width: 100%; margin-top: 20px; margin-bottom: 20px">
+    <n-flex justify="center">
+      <n-switch v-model:value="isVideo" size="large" @update:value="handleChange">
+        <template #checked>
+          摄像头已打开
+        </template>
+        <template #unchecked>
+          摄像头已关闭
+        </template>
+      </n-switch>
+      <n-switch v-model:value="isAudio" size="large" @update:value="handleChange">
+        <template #checked>
+          麦克风已打开
+        </template>
+        <template #unchecked>
+          麦克风已关闭
+        </template>
+      </n-switch>
+    </n-flex>
+    <div style="margin-top: 20px">
+      <n-flex justify="center">
+        <n-button @click="handleJoin">
+          join
+        </n-button>
+        <n-button @click="hangUp">
+          挂断
+        </n-button>
+        <n-button @click="show">
+          showDetails
+        </n-button>
+        <n-button @click="refresh">
+          刷新
+        </n-button>
+      </n-flex>
+    </div>
+  </n-layout-header>
   <div class="container">
-    <div ref="videoContainer">
-      <video ref="localVideo" autoplay />
-    </div>
-    <div>
-      <n-button @click="handleJoin">
-        join
-      </n-button>
-      <n-button @click="hangUp">
-        挂断
-      </n-button>
-      <n-button @click="show">
-        showDetails
-      </n-button>
-    </div>
+    <n-flex justify="center">
+      <div ref="videoContainer">
+        <video ref="localVideo" autoplay />
+      </div>
+    </n-flex>
   </div>
 </template>
 
--- src/components/Ws.vue
+++ src/components/Ws.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { useUserStore } from '~/store/modules/user'
-import { service, wssBase } from '~/api'
+import { wssBase } from '~/api'
+import { useWebsocketStore } from '~/store/modules/websocket'
 
 interface Message {
   messageId: number
@@ -21,21 +22,35 @@ export default defineComponent({
     const userStore = useUserStore()
     const route: any = useRoute()
 
-    async function queryHistoryMessages() {
-      const response = await service.get(`/messages/${route.params.server_id}/${route.params.name}`)
-      const { messageList } = response.data
-      messages.value = messageList
-      if (!messages.value)
-        window.$message.warning('该频道没有更多信息了')
+    const page = ref(1)
+    const pageSize = ref(7)
+    const pageCount = ref(1)
+    const websocketStore = useWebsocketStore()
+    const info = ref('')
+    async function getPagesMessages() {
+      messages.value = await websocketStore.getPagesMessages(page, pageCount, pageSize)
     }
-    function initWebsocket() {
+    onActivated(async () => {
+      await initWebsocket()
+    })
+    onBeforeRouteUpdate(async (to) => {
+      messages.value = await websocketStore.getPagesMessages(page, pageCount, pageSize, to)
+      await shutdown()
+    })
+    onBeforeRouteLeave(async (_to, _from, next) => {
+      await shutdown()
+      next()
+    })
+    async function initWebsocket() {
       socket = new WebSocket(`${wssBase}/api/wz?serverId=${route.params.server_id}&channelId=${route.params.name}&token=${encodeURIComponent(userStore.getToken)}`)
-      socket.onopen = () => {
+      socket.onopen = async () => {
         window.$message.success('连接成功')
-        queryHistoryMessages()
+        info.value = '你加入了频道'
+        messages.value = await websocketStore.getPagesMessages(page, pageCount, pageSize)
       }
       socket.onclose = () => {
         window.$message.error('连接已关闭')
+        info.value = '你断开了连接'
       }
     }
     function relinkWebsocket() {
@@ -50,12 +65,17 @@ export default defineComponent({
         messages.value.push(message)
       }
       if (messageText.value.trim() !== '') {
-        socket.send(JSON.stringify({ data: messageText.value }))
+        try {
+          socket.send(JSON.stringify({ data: messageText.value }))
+        }
+        catch (e) {
+          window.$message.error('连接断开或未连接')
+        }
         messageText.value = ''
       }
     }
 
-    function shutdown() {
+    async function shutdown() {
       try {
         if (socket)
           socket.close()
@@ -65,8 +85,8 @@ export default defineComponent({
         window.$message.error('发生错误')
       }
     }
+
     return {
-      queryHistoryMessages,
       sendMessage,
       messages,
       initWebsocket,
@@ -74,12 +94,27 @@ export default defineComponent({
       userId: userStore.getUserId,
       relinkWebsocket,
       shutdown,
+      page,
+      pageSize,
+      getPagesMessages,
+      pageCount,
+      info,
     }
   },
 })
 </script>
 
 <template>
+  <n-flex justify="center">
+    <n-pagination
+      v-model:page="page"
+      v-model:page-size="pageSize"
+      :page-sizes="[7, 10, 20]"
+      show-size-picker
+      :item-count="pageCount"
+      @update:page="getPagesMessages"
+    />
+  </n-flex>
   <ChatBubble
     v-for="(msg, index) in messages"
     :key="index"
@@ -89,11 +124,14 @@ export default defineComponent({
     :time="msg.sendDate"
     :username="msg.senderName"
   />
+  <n-flex justify="center">
+    {{ info }}
+  </n-flex>
+  <n-button :focusable="false" @click="getPagesMessages">
+    分页
+  </n-button>
   <n-button :focusable="false" @click="initWebsocket">
     加入频道
-  </n-button>
-  <n-button :focusable="false" @click="queryHistoryMessages">
-    Search
   </n-button>
   <n-button :focusable="false" @click="relinkWebsocket">
     Relink
