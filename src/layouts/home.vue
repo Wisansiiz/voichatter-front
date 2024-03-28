a0d8ce2d5b267ae840de38a272fc2803ded7998e
Wisansiiz <18257719723@163.com>
2024-03-28 22:41:25 +0800

    style: 样式

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -9,12 +9,17 @@
       [Home Layout]
     </div>
   </div>
+  <n-layout-footer>
+    <div mx-auto mt-5 text-center text-sm opacity-50>
+      [Home Layout]
+    </div>
+  </n-layout-footer>
 <!--  </n-flex> -->
 </template>
 
 <style scoped>
 .contain {
-  margin-top: 10%;
+  height: 80vh;
   background: #ffffff;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
