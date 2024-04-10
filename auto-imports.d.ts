bcb73cf02af376d9e0c1cf8fc189ff9a57686b75
Wisansiiz <18257719723@163.com>
2024-04-10 15:55:35 +0800

    chore: 依赖更新

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -101,7 +101,6 @@ declare global {
   const resolveComponent: typeof import('vue')['resolveComponent']
   const resolveRef: typeof import('@vueuse/core')['resolveRef']
   const resolveUnref: typeof import('@vueuse/core')['resolveUnref']
-  const sendMsg: typeof import('./src/composables/websocket')['sendMsg']
   const shallowReactive: typeof import('vue')['shallowReactive']
   const shallowReadonly: typeof import('vue')['shallowReadonly']
   const shallowRef: typeof import('vue')['shallowRef']
